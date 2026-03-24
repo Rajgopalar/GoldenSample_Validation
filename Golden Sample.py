95370f40dd443d68ad74da194a03d21026d3e27f
Rajgopal A R <rajgopalarmr7@gmail.com>
2026-03-24 15:35:14 +0530

    Update Golden Sample.py

--- Golden Sample.py
+++ Golden Sample.py
@@ -28,7 +28,7 @@ CC_RECIPIENTS = [
 
 # Auto email settings
 AUTO_EMAIL_HOUR = 15       # 24-hr format
-AUTO_EMAIL_MINUTE = 35
+AUTO_EMAIL_MINUTE = 36
 AUTO_EMAIL_ENABLED = True
 
 # ===================================
